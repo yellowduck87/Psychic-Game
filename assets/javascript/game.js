b7ed8c2a18403856c9e5b143d6cf522a5bf440fd
Megan Evans <megan.evans.ducky@gmail.com>
2018-05-27 20:28:42 -0600

    added some js ..meh

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var win = 0;
 var lose = 0;
-var lives = 9;
+var lives = 5;
 
 // game code
 
@@ -10,10 +10,10 @@ document.onkeyup = function () {
     var computerGuess = letters[Math.floor(Math.random() * letters.length)];
     console.log(computerGuess);
     
-    if (userGuess == computerGuess) {
+    while (lives > 0) {
+        if (userGuess == computerGuess) {
         alert("you win!");
         win++;
-    
         // set guesses left and subtract 1 for each incorrect guessed
         // record all incorrect guesses in the HTMl 
         // when remaining gueses reaches 0--player loses 
@@ -22,13 +22,15 @@ document.onkeyup = function () {
     }else{ 
         (userGuess !== computerGuess);
             lives--;
-            if (lives==0);
-                alert("you lose");
-        }
+    }      
+            
+    if (lives==0);
+        lose++;
+        alert("you lose");
     }
 
-    document.getElementById("wins")
-    document.getElementById("losses")
-    document.getElementById("lives")
-    document.getElementById("letters-guessed")
-
+    document.querySelector("#wins") = wins;
+    document.querySelector("#losses") = lose;
+    document.querySelector("#lives") = lives;
+    document.querySelector("#letters-guessed") = letters-guessed;
+}
