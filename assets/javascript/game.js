1ce364e22cdd2a6c5d81598de416270650fc3a4f
Megan Evans <megan.evans.ducky@gmail.com>
2018-05-30 08:39:45 -0600

    arrrg

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,50 @@
-var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var letters = ['a', 'b', 'c'];
+// 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 var win = 0;
 var lose = 0;
 var lives = 5;
+
 
 // game code
 
 document.onkeyup = function () {
     var lettersGuessed = String.fromCharCode(event.keyCode).toLowerCase();
     var computerGuess = letters[Math.floor(Math.random() * letters.length)];
+    // var guessedArray = [];
+    // guessedArray.push(lettersGuessed);
     console.log(computerGuess);
-    document.querySelector("#letters-guessed").innerHTML = "What you have guessed: " + lettersGuessed;
+    console.log(lettersGuessed);
 
-    while (lives > 0) {
+
+    while (lives >= 1) {
         if (lettersGuessed == computerGuess) {
-            alert("you win!");
             win++;
         }
-        if (lettersGuessed !== computerGuess) {
+        // document.querySelector("#letters-guessed").innerHTML = "What you have guessed: " + guessedArray;
+        // }
+        else if (lettersGuessed !== computerGuess) {
             lives--;
         }
-        // set guesses left and subtract 1 for each incorrect guessed
-        // record all incorrect guesses in the HTMl 
-        // when remaining gueses reaches 0--player loses 
-        // reset button   
+        //     // guessedArray = function() { 
+        //     //     guessedArray.push(lettersGuessed)
+        //     // };
+        //     // document.querySelector("#letters-guessed").innerHTML = "What you have guessed: " + guessedArray;
+        // }
+        // //     }
+        // //     // set guesses left and subtract 1 for each incorrect guessed
+        // //     // record all incorrect guesses in the HTMl 
+        // //     // when remaining gueses reaches 0--player loses 
+        // //     // reset button   
 
-       
+
     }
     if (lives == 0) {
         lose++;
-        alert("you lose");
+        alert("you lose, try again");
+        lives = 5;
     }
+
+    document.querySelector("#wins").innerHTML = "Wins: " + win;
+    document.querySelector("#losses").innerHTML = "Losses: " + lose;
+    document.querySelector("#lives").innerHTML = "Lives: " + lives;
 }
-document.querySelector("#wins").innerHTML = "Wins: " + win;
-document.querySelector("#losses").innerHTML = "Losses: " + lose;
-document.querySelector("#lives").innerHTML = "Lives: " + lives;
