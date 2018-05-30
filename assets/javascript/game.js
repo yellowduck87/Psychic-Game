2f5ca8d318e3ac9a9efccd7677a8fea18db72311
Megan Evans <megan.evans.ducky@gmail.com>
2018-05-30 09:05:32 -0600

    progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,4 @@
-var letters = ['a', 'b', 'c'];
-// 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var win = 0;
 var lose = 0;
 var lives = 5;
@@ -10,41 +9,50 @@ var lives = 5;
 document.onkeyup = function () {
     var lettersGuessed = String.fromCharCode(event.keyCode).toLowerCase();
     var computerGuess = letters[Math.floor(Math.random() * letters.length)];
-    // var guessedArray = [];
-    // guessedArray.push(lettersGuessed);
+    var computerAnswer = computerGuess;
+    // var guessedArray = []; 
+    // guessedArray = function() { 
+    //     guessedArray.push(lettersGuessed)
+    // }
+    console.log(computerAnswer);
     console.log(computerGuess);
     console.log(lettersGuessed);
 
 
-    while (lives >= 1) {
-        if (lettersGuessed == computerGuess) {
+    if (lives > 0) {
+        if (lettersGuessed == computerAnswer) {
+            alert("Congradulations! You Win!");
             win++;
+            lives = 5;
+            lettersGuessed = "";
+            // var guessedArray = [];
         }
-        // document.querySelector("#letters-guessed").innerHTML = "What you have guessed: " + guessedArray;
-        // }
-        else if (lettersGuessed !== computerGuess) {
-            lives--;
-        }
-        //     // guessedArray = function() { 
-        //     //     guessedArray.push(lettersGuessed)
-        //     // };
-        //     // document.querySelector("#letters-guessed").innerHTML = "What you have guessed: " + guessedArray;
-        // }
-        // //     }
-        // //     // set guesses left and subtract 1 for each incorrect guessed
-        // //     // record all incorrect guesses in the HTMl 
-        // //     // when remaining gueses reaches 0--player loses 
-        // //     // reset button   
-
-
+    
+    if (lettersGuessed !== computerAnswer) {
+        lives--;
     }
-    if (lives == 0) {
-        lose++;
-        alert("you lose, try again");
-        lives = 5;
-    }
+  
+    //     // };
+    //     // document.querySelector("#letters-guessed").innerHTML = "What you have guessed: " + guessedArray;
+    // }
+    // //     }
+    // //     // set guesses left and subtract 1 for each incorrect guessed
+    // //     // record all incorrect guesses in the HTMl 
+    // //     // when remaining gueses reaches 0--player loses 
+    // //     // reset button   
 
-    document.querySelector("#wins").innerHTML = "Wins: " + win;
-    document.querySelector("#losses").innerHTML = "Losses: " + lose;
-    document.querySelector("#lives").innerHTML = "Lives: " + lives;
+
+}
+if (lives == 0) {
+    lose++;
+    alert("you lose, try again");
+    lives = 5;
+    lettersGuessed = "";
+    // var guessedArray = [];
+}
+
+document.querySelector("#wins").innerHTML = "Wins: " + win;
+document.querySelector("#losses").innerHTML = "Losses: " + lose;
+document.querySelector("#lives").innerHTML = "Lives: " + lives;
+document.querySelector("#letters-guessed").innerHTML = "What you have guessed: " + lettersGuessed;
 }
