0183373c99e47b5c6063493b69326ca77bfd8050
Megan Evans <megan.evans.ducky@gmail.com>
2018-05-31 10:24:02 -0600

    completed game code and everything works as desired

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,57 +2,51 @@ var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
 var win = 0;
 var lose = 0;
 var lives = 5;
-
+var guessedArray = [];
+var computerGuess = letters[Math.floor(Math.random() * letters.length)];
 
 // game code
 
 document.onkeyup = function () {
     var lettersGuessed = String.fromCharCode(event.keyCode).toLowerCase();
-    var computerGuess = letters[Math.floor(Math.random() * letters.length)];
+
     var computerAnswer = computerGuess;
-    // var guessedArray = []; 
-    // guessedArray = function() { 
-    //     guessedArray.push(lettersGuessed)
-    // }
-    console.log(computerAnswer);
-    console.log(computerGuess);
-    console.log(lettersGuessed);
+    //player must choose an alphabetic letter
+    if (!letters.includes(lettersGuessed)) {
+        alert("Please pick a letter from the alphabet.")
+        //when player choses a letter, the game code begins
+    } else {
+        //letters typed are added to the screen
+        guessedArray.push(lettersGuessed)
+        if (lives > 0) {
+            if (lettersGuessed == computerAnswer) {
+                alert("Congradulations! You Win!");
+                //reset code
+                win++;
+                lives = 5;
+                guessedArray = [];
+                computerGuess = letters[Math.floor(Math.random() * letters.length)];
 
+            }
 
-    if (lives > 0) {
-        if (lettersGuessed == computerAnswer) {
-            alert("Congradulations! You Win!");
-            win++;
-            lives = 5;
-            lettersGuessed = "";
-            // var guessedArray = [];
+            if (lettersGuessed !== computerAnswer) {
+                lives--;
+            }
+
         }
-    
-    if (lettersGuessed !== computerAnswer) {
-        lives--;
+        if (lives == 0) {
+            lose++;
+            alert("you lose, try again");
+            //reset code
+            lives = 5;
+            guessedArray = [];
+            computerGuess = letters[Math.floor(Math.random() * letters.length)];
+
+        }
+        //updated variables are printed to the page
+        document.querySelector("#wins").innerHTML = "Wins: " + win;
+        document.querySelector("#losses").innerHTML = "Losses: " + lose;
+        document.querySelector("#lives").innerHTML = "Lives: " + lives;
+        document.querySelector("#letters-guessed").innerHTML = "What you have guessed: " + guessedArray;
     }
-  
-    //     // };
-    //     // document.querySelector("#letters-guessed").innerHTML = "What you have guessed: " + guessedArray;
-    // }
-    // //     }
-    // //     // set guesses left and subtract 1 for each incorrect guessed
-    // //     // record all incorrect guesses in the HTMl 
-    // //     // when remaining gueses reaches 0--player loses 
-    // //     // reset button   
-
-
-}
-if (lives == 0) {
-    lose++;
-    alert("you lose, try again");
-    lives = 5;
-    lettersGuessed = "";
-    // var guessedArray = [];
-}
-
-document.querySelector("#wins").innerHTML = "Wins: " + win;
-document.querySelector("#losses").innerHTML = "Losses: " + lose;
-document.querySelector("#lives").innerHTML = "Lives: " + lives;
-document.querySelector("#letters-guessed").innerHTML = "What you have guessed: " + lettersGuessed;
 }
